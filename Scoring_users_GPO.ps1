a6414a19a96033e79c69777ec7f6621bf8cf9f5d
Teamonymous <60900009+teamonymous@users.noreply.github.com>
2022-03-10 23:35:00 -0500

    Add files via upload

---
+++ Scoring_users_GPO.ps1
@@ -0,0 +1,7 @@
+﻿Write-Host "Hey Cam Lets get this dub, got a few questions for us to answer"
+
+$ou = Read-Host -Prompt 'Enter the comp users OU name'
+$DomainName = Read-Host 'Enter the domain name'
+$com = Read-Host 'Enter the extension (.com, .net, .local, etc.)'
+
+New-GPO -Name 'Comp Users' -Comment 'For Stupid Scoring Users' | New-GPLink -Target "OU=$ou,DC=$DomainName,DC=$com" -Enforced Yes
