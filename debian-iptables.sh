4dbfd2c68a4d89693fd6153285bac49df1615384
Teamonymous <60900009+teamonymous@users.noreply.github.com>
2022-03-10 20:22:26 -0500

    Update debian-iptables.sh

--- debian-iptables.sh
+++ debian-iptables.sh
@@ -25,11 +25,12 @@ iptables -P INPUT DROP
 iptables -P OUTPUT DROP
 iptables-save > /my.iptables.bck
 
-
-echo "#Disable IPv6
-net.ipv6.conf.all.disable_ipv6 = 1
-net.ipv6.conf.default.disable_ipv6 = 1
-net.ipv6.conf.lo.disable_ipv6 = 1" >> /etc/sysctl.conf
+if ! grep q "net.ipv6.conf.all.disable_ipv6 = 1" /etc/sysctl.conf; then
+  echo "#Disable IPv6
+  net.ipv6.conf.all.disable_ipv6 = 1
+  net.ipv6.conf.default.disable_ipv6 = 1
+  net.ipv6.conf.lo.disable_ipv6 = 1" >> /etc/sysctl.conf
+fi
 
 service procps restart
 service procps status 
